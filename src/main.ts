5aed9a203fd74ece6ea3cea852b0ed5780948e4b
trantuanhung <23659581.hung@student.iuh.edu.vn>
2026-06-12 18:41:13 +0700

    update: cors for quizzes

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,9 @@ async function bootstrap() {
   app.useGlobalInterceptors(app.get(RequestLoggingInterceptor), new ResponseInterceptor());
 
   app.enableCors({
-    origin: [/^https?:\/\/[^.]+\.uside\.studio$/, 'http://localhost:3000', 'http://localhost:5173', 'http://localhost:5500', 'https://profiles-uside-studio.vercel.app'],
+    origin: [/^https?:\/\/[^.]+\.uside\.studio$/, 'http://localhost:3000', 'http://localhost:5173', 'http://localhost:5500', 'https://profiles-uside-studio.vercel.app', 
+      'https://quizzes-uside-studio.vercel.app', 
+    ],
     credentials: true,
   });
   
